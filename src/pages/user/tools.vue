a42074e21a57711d16ed94ab0e1b1fbb0cbee551
xpchen <357437579@qq.com>
2018-03-20 19:40:33 +0800

    道具和宝藏

--- src/pages/user/tools.vue
+++ src/pages/user/tools.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="rank-page">
+  <div class="tool-page-content">
     <!-- head begin -->
     <div class="main-title-box">
       <span></span>
@@ -7,23 +7,32 @@
     <router-link to="/checkpoint/list" class="back-box"></router-link>
     <!-- head end -->
 
-    <div class="setting-page-content">
-      <div v-for="tool in tools_list" :key="tool.id">
-        <div>
-          <img :src="tool.question.content" style="width:100%">
-          <span v-if="tool.count>1">{{tool.count}}</span>
-        </div>
-      </div>
-
-
-      <div>
-        <p>个人成绩</p>
-        <span>成绩：</span>{{$store.state.record_list.mark}}分
-        <span>用时：</span>{{$store.state.record_list.time}}
-        <span>距离：</span>{{$store.state.record_list.mark}}米
+    <div class="tool-box">
+      <span class="tool-page-title">我的道具卡</span>
+      <div v-for="tool in tools_list" :key="tool.id" class="tool-card">
+        <img v-if="tool.count>1" :src="tool.question.content">
+        <img v-else src="./images/treasure.png">
+        <span v-if="tool.count>1">{{tool.count}}</span>
       </div>
     </div>
 
+    <div class="treasure-box">
+      <span class="tool-page-title">我的宝藏卡</span>
+      <div v-for="tool in treasure_list" :key="tool.id" class="treasure-card">
+        <img v-if="tool.count>1" :src="tool.question.content">
+        <img v-else src="./images/treasure.png">
+        <span v-if="tool.count>1">{{tool.count}}</span>
+      </div>
+    </div>
+
+    <div class="data-box">
+      <span class="tool-page-title">我的数据</span>
+      <div class="data">
+        <span class="myMark">成绩：</span>{{$store.state.record_list.mark}}分
+        <span class="myTime">用时：</span>{{$store.state.record_list.time}}
+        <span class="myDistance">距离：</span>{{$store.state.record_list.mark}}米
+      </div>
+    </div>
   </div>
 </template>
 
@@ -33,46 +42,47 @@ import { Indicator } from "mint-ui";
 export default {
   data() {
     return {
-      tools_list: []
+      tools_list: {
+        a01: { id:'a01', count: 0, question: null },
+        a02: { id:'a02', count: 0, question: null },
+        a03: { id:'a03', count: 0, question: null },
+      },
+      treasure_list: {
+        b01: { id:'b01', count: 0, question: null },
+        b02: { id:'b02', count: 0, question: null },
+        b03: { id:'b03', count: 0, question: null },
+      },
     };
   },
   created() {
-    let temp_list = this.$store.state.record_list.tools;
+    let tools_list = this.$store.state.record_list.tools;
     let question_list = this.$store.state.game_config.question_list;
-    let tools_list = [];
 
     // 将道具添加到列表
-    for (let key in temp_list) {
-      let qid = temp_list[key];
+    for (let key in tools_list) {
+      let qid = tools_list[key];
       let question = question_list[qid];
       if (question == undefined) {
         continue;
       }
-      if (question.items != "tool") {
-        continue;
-      }
 
-      // 检查是否存在道具
-      let tid = this.get_tools(tools_list, question.id);
-      if (tid == -1) {
-        let tool = {
-          question: question,
-          count: 1
-        };
-        tools_list.push(tool);
-      } else {
-        let tool = tools_list[tid];
-        tool.count++;
+      // 对道具进行识别
+      if (question.items == "tool") {
+        this.get_tools(this.tools_list, question);
+      }
+      else if (question.items == "treasure") {
+        this.get_tools(this.treasure_list, question);
       }
     }
-    this.tools_list = tools_list;
   },
   mounted() {},
   methods: {
-    get_tools(tools_list, id) {
+    get_tools(tools_list, question) {
       for (let key in tools_list) {
         let tool = tools_list[key];
-        if (tool.question.id == id) {
+        if (tool.id == question.answer) {
+          tool.count++;
+          tool.question = question;
           return key;
         }
       }
