a8cc34beff3e8c351bb77332cc7cd7995e19378c
钟诚 <37396236+cdzcheng@users.noreply.github.com>
2018-07-05 15:34:14 +0800

    增加了一个装饰用的div
    div class=decorate

--- src/pages/checkpoint/show.vue
+++ src/pages/checkpoint/show.vue
@@ -8,6 +8,8 @@
       <router-link to="/checkpoint/list" class="back-box"></router-link>
       <span class="help-box" @click="show_help"></span>
       <!-- head end -->
+      
+      <div class="decorate"></div>
 
       <div @click="next_page">
         <div class="task-info" v-html="html"></div>
